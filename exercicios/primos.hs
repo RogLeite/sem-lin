1dab8bd2a6b787b4b7d82bbd4aa40457ad54e8b4
RogLeite <rodrigo.ldasilva@hotmail.com>
2019-04-14 10:44:37 -0300

    Lista de primos está pronta, precisa otimizar, por isso criei o operador /*|>

---
+++ exercicios/primos.hs
@@ -0,0 +1,31 @@
+inf_int:: [Integer]
+inf_int = aux 1
+    where 
+        aux x = x:(aux (x+1))
+
+at:: Integer -> [Integer] -> Integer
+at _ [] = 0
+at 1 (x:xs) = x
+at a (x:xs) = at (a-1) xs
+
+first::[Integer] -> Integer
+first = at 1
+
+--OPERATOR--NOT MULTIPLE-------------------------------------
+(/*):: Integer -> Integer -> Bool
+(/*) x a = (x `mod` a) /= 0
+
+--OPERATOR--/* OR LARGER---------------------------------------
+(/*|>):: Integer -> Integer -> Bool
+(/*|>) x a = (x > a) || (x /* a)
+
+--PRIMOS----------------------------------------------------
+primos:: [Integer]
+primos = aux 2 inf_int
+    where
+        aux a list = a:(aux (first filtered) filtered)
+            where
+                --filtered é a lista infinita com todos os multiplos de primos menores que a filtrados
+                filtered = filter ( /* a ) (filter (> a) list)
+                
+                

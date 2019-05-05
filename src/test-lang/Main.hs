74750d9a0cbc711a60793f5673428aa96e13a1f4
RogLeite <rodrigo.ldasilva@hotmail.com>
2019-05-05 18:22:05 -0300

    Lang Funcionagit status!

--- src/test-lang/Main.hs
+++ src/test-lang/Main.hs
@@ -1,3 +1,8 @@
 import Lang
-main = do 
-    
+mainMem:: Mem
+mainMem = initMem
+
+main = do putStrLn "Insert a number. If /= 0 then 1 else 2"
+          num <- getLine
+          let number = read num :: Integer
+          putStrLn (show ( evalCmd (CmdSeq (CmdAsg "a" (ExpNum number)) (CmdIf (ExpVar "a") (CmdAsg "b" (ExpNum 1)) (CmdAsg "b" (ExpNum 2))) ) mainMem  "b" ) )
